9d6f630893e95c7b398d50f06554d64bd44fc935
jeong won <jw221356@naver.com>
2024-07-23 16:21:19 +0900

    feat : 데이터 가져오기, 검색창, 전체적인 레이아웃 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(posts => setPosts(posts));
+  }, []);
+
   return (
-    <div >
-     
+    <div style={{ padding: "1rem" }}>
+      <input 
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+
+      <div style={{ display: "flex" }}>
+        <A message={value} posts={posts} />
+        <B message={value} posts={posts} />
+      </div>
     </div>
   );
 }
