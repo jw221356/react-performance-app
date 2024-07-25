ae4e7e38219da98ec3088c616d92099f87bd2db3
jeong won <jw221356@naver.com>
2024-07-25 13:35:13 +0900

    B component 생성

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 
       <div style={{ display: "flex" }}>
         <A message={value} posts={posts} />
-        <B />
+        <B message={value} posts={posts} />
       </div>
     </div>
   );
